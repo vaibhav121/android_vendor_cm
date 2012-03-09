1f061ef340dfef2d9e07e1e6dcefc9c1d96ff380
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-03-09 10:38:31 +0200

    Configure selected ringtones from device folder
    Change-Id: I559914abb02b5739a9dc492f865c2cf3b28149bc

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -14,10 +14,10 @@ $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 include vendor/cm/config/cm_audio.mk
 
 # Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=CyanTone.ogg \
-    ro.config.notification_sound=CyanMessage.ogg \
-    ro.config.alarm_alert=CyanAlarm.ogg
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.config.ringtone=CyanTone.ogg \
+#    ro.config.notification_sound=CyanMessage.ogg \
+#    ro.config.alarm_alert=CyanAlarm.ogg
 
 PRODUCT_PACKAGES += \
   Mms
