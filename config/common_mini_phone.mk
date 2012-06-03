01efe62ccc90a8b355218da81c1129447c3d4237
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-06-03 12:10:01 +0300

    Use custom config for mini_phone
    Change-Id: I30825c6295df488cc2af38b4ab693584a7bc4aee

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -7,16 +7,6 @@ include frameworks/base/data/sounds/NewAudio.mk
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=CyanTone.ogg \
-    ro.config.notification_sound=CyanMessage.ogg \
-    ro.config.alarm_alert=CyanAlarm.ogg
-
 PRODUCT_PACKAGES += \
   Mms
 
-ifeq ($(TARGET_BOOTANIMATION_NAME),)
-    PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/vertical-320x480.zip:system/media/bootanimation.zip
-endif
